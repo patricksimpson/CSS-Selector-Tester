fc67f1662125d8b72780789403f55a9e3d381b7e
Patrick Simpson <izerop@gmail.com>
2016-10-29 15:24:49 -0400

    feat: general ui and functionality improvements

--- code/nScript.js
+++ code/nScript.js
@@ -19,9 +19,11 @@ var nd___vo = {
     $("body").append("<div id='nd___box'></div>");
     $("#nd___box").append("<button id='nd___close'>X</button><span id='nd___title'>CSS Selector Tester</span>");
     $("#nd___box").append("<form action='#' id='nd___form'></form>");
-    $("#nd___form").append("<p><label>Selector: </label><input type='text' id='nd___selector' /> <a id='nd___clean'>clear</a></p>");
+    $("#nd___form").append("<p><input type='text' id='nd___selector' placeholder='Selector' /></p><p><a id='nd___clean'>Clear Selector</a></p>");
     $("#nd___form").append("<p>Element: <span id='nd___status'>undefined</span></p>");
-    $("#nd___form").append("<p class='squaredFour'><input type='checkbox' id='nd___ohighlight' checked='true' /><label for='nd___ohighlight'>Highlight</label></p>");
+    $("#nd___form").append("<p class='checkbox'><input type='checkbox' id='nd___ohighlight' checked='true' /><label for='nd___ohighlight'>Highlight</label></p>");
+  },
+  observe: function() {
     $("#nd___form").on("submit", function(){ return false; });
     $("#nd___selector").keyup(function(){nd___vo.go();});
     $("#nd___box").draggable({ opacity: 0.35 }).resizable({ minWidth: 305 , minHeight: 155 });
@@ -29,23 +31,24 @@ var nd___vo = {
       nd___vo.alert("undefined");
       nd___vo.clean();
       $("#nd___selector").val("");
+      nd___vo.cacheE = null;
     });
     $("#nd___close").click(function(){
       nd___vo.alert("undefined");
       nd___vo.remove();
       $("#nd___selector").val("");
     });
-    $("#nd___ohighlight").click(function(){
-      if($("#nd___ohighlight").attr("checked") !== "checked"){
-        nd___vo.clean();
-      }else{
-        if(nd___vo.cacheE){
-          $(nd___vo.cacheE).css("outlineColor", "RGB(204,0,0)");
-          $(nd___vo.cacheE).css("outlineStyle", "dashed");
-          $(nd___vo.cacheE).css("outlineWidth", "2px");
-          $(e).addClass("nd___highlighted");
+    $('#nd___ohighlight').change(function() {
+        if (!this.checked) {
+          nd___vo.clean();
+        } else {
+          if (nd___vo.cacheE != null){
+            $(nd___vo.cacheE).css("outlineColor", "RGB(204,0,0)");
+            $(nd___vo.cacheE).css("outlineStyle", "dashed");
+            $(nd___vo.cacheE).css("outlineWidth", "2px");
+            $(nd___vo.cacheE).addClass("nd___highlighted");
+          }
         }
-      }
     });
   },
   go: function(){
@@ -121,4 +124,5 @@ if($("#nd___box")[0]){
   nd___vo.show();
 }else{
   nd___vo.add();
+  nd___vo.observe();
 }
